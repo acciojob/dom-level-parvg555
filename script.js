b62219ba31313361513551af1c77e4a29d74fd1e
Parv Gupta <Parvg555@gmail.com>
2023-02-27 17:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+//your code here
+
+// Get the element you want to find the level of
+let element = document.getElementById("level");
+
+// Initialize the level variable
+let level = 0;
+
+// Iterate through the parent nodes of the element
+while (element.parentNode) {
+    // Increase the level by 1 for each parent node
+    level++;
+    // Update the element to the parent node
+    element = element.parentNode;
+}
+
+alert("The level of the element is: " + level);
